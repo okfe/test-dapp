d5a06c48f14910489a3096ece926097feb98f7f5
shaowei.zhou <shaowei.zhou@okg.com>
2024-08-09 16:28:19 +0800

    chore: Update npm dependencies and add CodeBox component

--- src/container/WalletAPI/BTC/connect/index.tsx
+++ src/container/WalletAPI/BTC/connect/index.tsx
@@ -1,8 +1,11 @@
 import APIButton from '@/components/common/APIButton';
+import CodeBox from '@/components/common/CodeBox';
 import Connector from '@/components/common/Connector';
 import PreviewBox from '@/components/common/PreviewBox';
 import { Col, Row } from 'antd';
 import React, { useState } from 'react';
+
+const demo = 'const result = await okxwallet.bitcoin.connect()';
 
 const ConnectSmart: React.FC = () => {
   const [data, setData] = useState({});
@@ -17,6 +20,7 @@ const ConnectSmart: React.FC = () => {
           <Connector onError={onCallback} />
         </Row>
         <APIButton apiName="connect" onCallback={onCallback} />
+        <CodeBox text={demo} />
       </Col>
       <Col span={12}>
         <PreviewBox value={data} />
