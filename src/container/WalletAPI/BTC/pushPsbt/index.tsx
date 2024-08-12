0fe13dcd94c374056e051941dd9bd84e27580e0e
shaowei.zhou <shaowei.zhou@okg.com>
2024-08-12 14:43:46 +0800

    feat: add codeBox for pushPsbt

--- src/container/WalletAPI/BTC/pushPsbt/index.tsx
+++ src/container/WalletAPI/BTC/pushPsbt/index.tsx
@@ -1,18 +1,30 @@
 import APIButton from '@/components/common/APIButton';
+import CodeBox from '@/components/common/CodeBox';
 import Connector from '@/components/common/Connector';
 import PreviewBox from '@/components/common/PreviewBox';
+
 import { Col, Input, Row, Space } from 'antd';
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 
 const PushPsbtSmart: React.FC = () => {
   const [result, setResult] = useState({});
   const [signedPsbt, setSignedPsbt] = useState('');
+
   const onCallback = async (result: object) => {
     setResult(result);
   };
   const onChange = (e) => {
     setSignedPsbt(e.target.value);
   };
+
+  const demo = useMemo(() => {
+    return `try {
+        let res = await okxwallet.bitcoin.pushPsbt('${signedPsbt ? signedPsbt : '70736274ff01007d...'}');
+          console.log(res);
+        } catch (e) {
+          console.log(e);
+        }`;
+  }, [signedPsbt]);
 
   return (
     <Row justify="space-between">
@@ -22,13 +34,14 @@ const PushPsbtSmart: React.FC = () => {
           <Input
             value={signedPsbt}
             onChange={onChange}
-            placeholder="填写PSBT"
+            placeholder="填写 PSBT"
           />
           <APIButton
             apiName="pushPsbt"
             onCallback={onCallback}
             params={[signedPsbt]}
           />
+          <CodeBox text={demo} />
         </Space>
       </Col>
       <Col span={12}>
