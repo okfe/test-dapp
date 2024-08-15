bc689e222563a33f57948b8bd52b207723ee9b9d
shaowei.zhou <shaowei.zhou@okg.com>
2024-08-15 15:22:46 +0800

    feat: change avatar in the initial state

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,14 @@
 // 运行时配置
 
 // 全局初始化数据配置，用于 Layout 用户信息和权限初始化
-export async function getInitialState(): Promise<{ name: string }> {
-  return { name: 'OKXer' };
+export async function getInitialState(): Promise<{
+  name: string;
+  avatar: string;
+}> {
+  return {
+    name: 'OKXer',
+    avatar: 'https://static.okx.com/cdn/assets/imgs/248/0125BBCCBD2DB8E0.png',
+  };
 }
 
 export const layout = () => {
